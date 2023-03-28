bbde2f572b21a208b4ad58f390aa4ba6a98183b3
LEvik1 <levik369strigo@gmail.com>
2023-03-28 15:49:17 +0500

    commit

---
+++ script.js
@@ -0,0 +1,121 @@
+// Отфильтровать задачи в переменные а и b
+    // Сохранить количество в ключе count
+    // Сохранить сами задачи в массиве-ключе arr
+    let arr = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut au`tem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+  
+            "completed": true
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        {
+            "userId": 1,
+   
+            "id": 9,
+            "title": "molestiae perspiciatis ipsa",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 10,
+            "title": "illo est ratione doloremque quia maiores aut",
+            "completed": true
+        },
+    ]
+    let a = {
+        count: 0,
+        arr: []
+    }
+   
+    let b = {
+        count: 0,
+        arr: []
+    }
+
+let c = {
+arr:[]
+}
+
+
+
+
+arr.filter(item =>{
+if(item.completed === true){
+    a.arr.push(item)
+     
+}
+if(item.completed === false){
+    b.arr.push(item)
+    
+}
+c.arr.push(item.completed)
+
+})
+
+console.log(c);
+
+
+console.log(a,b);
+
+
+
+
+
+
+
+
+let arr2 = [[{a : {price : 20}}],[{a : {price : 35}}],[{a : {price : 44}}]]
+let total = 0
+
+
+arr2.filter(item =>{
+        total = item.reduce((a,b)=>a + b)
+    
+})
+
+
+
+console.log(total);
